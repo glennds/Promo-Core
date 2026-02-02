e2ab16bca751ab93a2a5beeb34f2d5b543e10688
glennds <glenn@digishock.com>
2026-02-02 22:46:03 +0100

    Added function: woo-min-order-amount.php

---
+++ managepromo-core/functions/woo-min-order-amount.php
@@ -0,0 +1,10 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {exit;}
+if (!function_exists('managepromo_is_enabled') || !managepromo_is_enabled('woo_min_order_amount')) {return;}
+
+//////////////////////////////////
+// Function contents start HERE //
+//////////////////////////////////
+
+
+
